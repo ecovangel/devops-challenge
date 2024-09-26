ca76de4f5c0b27c3372e97714ad370fe825b4211
ecovangel <okaforevangel@yahoo.com>
2024-09-26 17:21:28 +0100

    Create variable.tf

---
+++ challenge-3/variable.tf
@@ -0,0 +1,31 @@
+# Variables File (variables.tf)
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "Public subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "Private subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+}
+
+
+variable "name" {
+  description = "The name used for the resources"
+  type        = string
+  default     = "ecovangel-skyward"  # Provide a default or pass it during apply
+}
